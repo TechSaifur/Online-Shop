31bc758dd7a1f2f8702f4a9685a38109fe1970c3
Saif Rahman <saifrahman.tech@gmail.com>
2023-12-26 16:48:48 +0530

    changed cart UI and gave functionality to it

--- backend/products.js
+++ backend/products.js
@@ -17,8 +17,15 @@ const products = [
     id: 3,
     name: "Galaxy S23 ultra",
     desc: "6.5-inch display",
-    price: 399,
+    price: 499,
     img: "https://tse2.mm.bing.net/th?id=OIP.E_NUOR-N7YRtQxXe1usphAHaHa&pid=Api&P=0&h=180",
+  },
+  {
+    id: 4,
+    name: "OnePlus 9 Pro",
+    desc: "6.7-inch display",
+    price: 399,
+    img: "https://media.whatprice.com/wp-content/uploads/2021/03/24053811/oneplus-9pro-pine-green-1261x1536.jpg",
   },
 ];
 
